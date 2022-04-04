105630b8e61491cba7225ac3681f08fd4a50b37c
Santiago <snarinocardenas@gmail.com>
2022-04-04 17:06:15 -0500

    Final Add Task

--- src/app/add-new-task/add-new-task.page.ts
+++ src/app/add-new-task/add-new-task.page.ts
@@ -7,14 +7,34 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./add-new-task.page.scss'],
 })
 export class AddNewTaskPage implements OnInit {
+  categories = ['Trabajo','Personal','Hogar'];
 
+  taskName;
+  taskDate;
+  taskPriority;
+  taskCategory;
+
+  taskObj;
   constructor(public modalCtrl: ModalController) { }
 
   ngOnInit() {
   }
 
   async cancel(){
-    await this.modalCtrl.dismiss();
+    await this.modalCtrl.dismiss(this.taskObj);
+  }
+
+  selectCat(index){
+    this.taskCategory = this.categories[index];
+  }
+
+  addTask(){
+    this.taskObj = ({itemName: this.taskName,
+                     itemDate: this.taskDate,
+                     itemPriority: this.taskPriority,
+                    itemCategory: this.taskCategory});
+
+    this.cancel();
   }
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,26 +9,7 @@ import { AddNewTaskPage } from '../add-new-task/add-new-task.page';
 })
 export class HomePage {
 
-  todoList = [
-    {
-    itemName : 'Mercado',
-    itemDate : '10/05/2022',
-    itemPriority: 'high',
-    itemCategory : 'Personal'
-  },
-    {
-    itemName : 'Codificar',
-    itemDate : '02/07/2022',
-    itemPriority: 'middle',
-    itemCategory : 'Trabajo'
-  },
-    {
-    itemName : 'Ropa',
-    itemDate : '05/13/2022',
-    itemPriority: 'Low',
-    itemCategory : 'Personal'
-  }
-];
+  todoList = [];
 
   today: number = Date.now();
   constructor(public modalCtrl: ModalController) {}
@@ -36,6 +17,11 @@ export class HomePage {
   async addTask(){
     const modal = await this.modalCtrl.create({
       component: AddNewTaskPage
+    });
+
+    modal.onDidDismiss().then(newTaskObj =>{
+      console.log(newTaskObj.data);
+      this.todoList.push(newTaskObj.data);
     });
 
     return await modal.present();
